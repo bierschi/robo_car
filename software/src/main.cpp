26be18f115d66ba902c3be2958276c3aafe8ffa0
Christian Bierschneider <christian.bierschneider@web.de>
2018-08-26 19:54:19 +0200

    Added opencv include

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "cstdio"
+#include "opencv2/opencv.hpp"
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    cv::VideoCapture cap(0);
     return 0;
 }
