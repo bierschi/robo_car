1415483af874b3eea877ea5c1366e79c4a547f3b
Christian Bierschneider <christian.bierschneider@web.de>
2018-08-29 19:25:28 +0200

    Added stream server for picamera

--- software/testing/robo_car_picamera.py
+++ software/testing/robo_car_picamera.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
-from picamera import PiCamera
+import socket
+import time
+import picamera
 
 
 def main():
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
-    camera.start_preview()
+    camera.framerate = 24
+
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.listen(0)
+
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('wb')
+    try:
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+    finally:
+        connection.close()
+        server_socket.close()
 
 
 if __name__ == '__main__':
     main()
+
