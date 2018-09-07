11594d13142c173bfc021f3366a3bdbc1a31558c
Christian Bierschneider <christian.bierschneider@web.de>
2018-09-07 14:00:15 +0200

    Test servo motor

--- software/testing/servomotor_steering.py
+++ software/testing/servomotor_steering.py
@@ -12,7 +12,7 @@ class ServoMotor:
         self.servo_min = servo_min
         self.servo_max = servo_max
 
-        self.freq = 60
+        self.freq = freq
         self.pwm.set_pwm_freq(self.freq)
 
     def move_extremas(self):
@@ -23,13 +23,26 @@ class ServoMotor:
             self.pwm.set_pwm(0, 0, self.servo_max)
             sleep(1)
 
+    def drive_left(self):
+        self.pwm.set_pwm(0, 0, self.servo_min)
+
+    def drive_right(self):
+        self.pwm.set_pwm(0, 0, self.servo_max)
+
+    def drive_straight_ahead(self):
+        self.pwm.set_pwm(0, 0, self.servo_max-self.servo_min)
+
 
 def main():
 
     print("Servo Motor test: ")
 
     sm = ServoMotor(140, 490, 60)
-    sm.move_extremas()
+    sm.drive_left()
+    sleep(1)
+    sm.drive_right()
+    sleep(1)
+    sm.drive_straight_ahead()
 
 
 if __name__ == '__main__':
