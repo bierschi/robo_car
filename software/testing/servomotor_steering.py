282654f63c8440b4f6afe9ca778254f6e3f07788
christian bierschneider <christian.bierschneider@web.de>
2018-09-07 18:42:58 +0000

    Changed servomotor_steering.py

--- software/testing/servomotor_steering.py
+++ software/testing/servomotor_steering.py
@@ -47,17 +47,19 @@ def main():
     print("Servo Motor test: ")
 
     sm_steering = ServoMotor(140, 490, channel=0, freq=60)
-    sm_camera = ServoMotor(140, 490, channel=1, freq=60)
-
+    sm_camera = ServoMotor(170,560, channel=15, freq=60)
+    #sm_steering.move_extremas()
+    #sm_camera.move_extremas()
+    sleep(2)
     sm_steering.drive_left()
     sm_camera.drive_left()
-    sleep(1)
+    sleep(2)
     sm_steering.drive_right()
     sm_camera.drive_right()
-    sleep(1)
+    sleep(2)
     sm_steering.drive_straight_ahead()
     sm_camera.drive_straight_ahead()
-
+    sleep(2)
 
 if __name__ == '__main__':
     main()
