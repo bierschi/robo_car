80f18e6da33d0fd333494c6ae9620419c11cf8e1
Christian Bierschneider <christian.bierschneider@web.de>
2018-09-12 19:54:29 +0200

    Changed project structure

---
+++ software/client/include/ClientSocket.h
@@ -0,0 +1,29 @@
+//
+// Created by christian on 03.09.18.
+//
+
+#ifndef ROBOCAR_CLIENTSOCKET_H
+#define ROBOCAR_CLIENTSOCKET_H
+
+#include "Socket.h"
+
+/**
+ * /CLASS ClientSocket
+ *
+ * creates a ClientSocket object
+ */
+class ClientSocket : private Socket {
+
+public:
+
+    ClientSocket(std::string host, int port);
+    virtual ~ClientSocket(){};
+
+    const ClientSocket& operator << (const std::string&) const;
+    const ClientSocket& operator << (Commands&) const;
+
+    const ClientSocket& operator >> (std::string&) const;
+    const ClientSocket& operator >> (Commands&) const;
+};
+
+#endif //ROBOCAR_CLIENTSOCKET_H
