d05a675a558bc9636dd537d4d2245e0a820f0960
Christian Bierschneider <christian.bierschneider@web.de>
2018-09-12 19:53:53 +0200

    Changed project structure

--- software/robocar/src/robocar_main.cpp
+++ software/robocar/src/robocar_main.cpp
@@ -1,11 +1,36 @@
 //
-// Created by christian on 06.09.18.
+// Created by christian on 03.09.18.
 //
 
+#include <iostream>
+#include <thread>
+#include "cstring"
+#include "cstdio"
+#include "comm/ServerSocket.h"
+#include "comm/SocketException.h"
+
+#define LOGGING true
 
 
-int main(int argc, char* argv[]) {
+int main() {
+    std::cout << "Test ServerSocket" << std::endl;
 
+    if (!LOGGING) {
+        std::clog.setstate(std::ios_base::failbit);
+    }
+
+    try {
+
+        ServerSocket server (2501, 1);
+        server.multipleClients();
+
+    }
+    catch ( SocketException& e) {
+
+        std::cout << "Exception was caught: " << e.description() << std::endl;
+
+    }
 
     return 0;
+
 }
