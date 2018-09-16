225c5e9c53099800fad8eb74a3cfad6f93bd6a99
Christian Bierschneider <christian.bierschneider@web.de>
2018-09-16 02:04:25 +0200

    Added changes in Ultrasonic.h

--- software/robocar/include/sensors/Ultrasonic.h
+++ software/robocar/include/sensors/Ultrasonic.h
@@ -5,9 +5,27 @@
 #ifndef ROBOCAR_ULTRASONIC_H
 #define ROBOCAR_ULTRASONIC_H
 
+#include <wiringPi.h>
+#include <time.h>
+#include <unistd.h>
+#include <chrono>
 
+/**
+ * /CLASS Ultrasonic
+ *
+ * creates a Ultrasonic object to trigger measurements from an ultrasonic range module
+ */
 class Ultrasonic {
 
+private:
+    int Trigger, Echo;
+
+public:
+    Ultrasonic(int TriggerPin, int EchoPin);
+    ~Ultrasonic();
+
+    double triggerOneMeasurement();
+    void startMeasurement();
 
 };
 
